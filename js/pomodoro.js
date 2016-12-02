a90baa65c10cb1e5028ca3e34d7d7b95752fb56e
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2016-12-02 18:34:23 -0500

    Completed Initial Project

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,48 @@
+var clock = document.getElementById('timer');
+var deadline, timeinterval, alarminterval,  status;
+var audio = new Audio('assets/bell.mp3');
+
+status = 0;
+
+function startTimer(type){
+  console.log(status);
+  if(status==1){
+    //Timer already running. Confirm to change timer and then clear the current one
+    ans = confirm("Are you sure you want to cancel the current Timer?");
+    console.log(ans);
+    if(ans==1){
+        status = 0;
+        clearInterval(timeinterval);
+    }
+    else{
+      return;
+    }
+  }
+
+  status = 1
+  mins = 0
+  if(type == 'pomodoro')
+    mins = 25
+  else if(type == 'shortB')
+    mins = 5
+  else if (type == 'longB')
+    mins = 10
+  console.log(mins);
+  deadline = new Date(Date.parse(new Date()) + mins * 60 * 1000);
+  //deadline = new Date(Date.parse(new Date()) + 10 * 1000);
+  updateClock();
+  timeinterval = setInterval(updateClock, 1000);  
+}
+
+function stopTimer(){
+  status = 0;
+  audio.pause();
+  clearInterval(timeinterval);
+  clearInterval(alarminterval);
+  clock.innerHTML = "00:00"
+  console.log("Timer stopped!");
+}
+
 function getTimeRemaining() {
   var t = Date.parse(deadline) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -9,69 +54,29 @@ function getTimeRemaining() {
   };
 }
 
-function startBreak(){
-  //deadline = new Date(Date.parse(new Date()) + 5 * 60 * 1000);
-  //audio.pause();
-  clearInterval(timeinterval);
-  deadline = new Date(Date.parse(new Date()) + 5 * 1000);
-  type=1;
-  updateClock();
-  timeinterval = setInterval(updateClock, 1000);
-}
-
 function updateClock() {
   var t = getTimeRemaining();
-  console.log(type);
-  minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-  secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-  console.log(t.total);
-
+  minsRemain = ('0' + t.minutes).slice(-2);
+  secsRemain = ('0' + t.seconds).slice(-2);
+  updateTime = minsRemain + ":" + secsRemain;
+  clock.innerHTML = updateTime
   if(t.total == 0){
+      status = false;
+      clearInterval(timeinterval);
+      triggerAlarm();
+  }
+}
+
+function triggerAlarm(){
+  audio = new Audio('assets/bell.mp3');  
+  count = 1
+  alarminterval = setInterval(function(){
       audio = new Audio('assets/bell.mp3');
-  }
-
-  if (t.total <= 0) {
-    //clearInterval(timeinterval);
-    minutesSpan.innerHTML = ('00');
-    audio.play();
-    if(type==0){
-      //Enable the Start Break Button
-    }else{
-      //Disable the Start Break Button
-    }
-  }
-
-  if(t.total <= -10000){
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-3);
-  }
-}
-
-var clock = document.getElementById('clockdiv');
-var deadline, timeinterval;
-var minutesSpan = clock.querySelector('.minutes');
-var secondsSpan = clock.querySelector('.seconds');
-var breakLeft = false;
-var type;
-var audio = new Audio('assets/bell.mp3');
-minutesSpan.innerHTML = ('00');
-secondsSpan.innerHTML = ('00');
-
-function startTimer(){
-  //deadline = new Date(Date.parse(new Date()) + 25 * 60 * 1000);
-  deadline = new Date(Date.parse(new Date()) + 10 * 1000);
-
-  //Disable the Start Timer Button
-  type=0;
-  updateClock();
-  timeinterval = setInterval(updateClock, 1000);
-}
-
-function stopTimer(){
-
-  //Enable the Start Timer Button
-  audio.pause();
-  clearInterval(timeinterval);
-  minutesSpan.innerHTML = ('00');
-  secondsSpan.innerHTML = ('00');
+      audio.play();
+      count++;
+      if (count==5){
+        clearInterval(alarminterval);
+      }
+  }, 3000);
+  audio.play();
 }
